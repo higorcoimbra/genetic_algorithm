64b74f1fa1f872d3b1f6e06823e7445d60aeb1f4
Higor Coimbra <higor1308@gmail.com>
2017-10-27 12:10:02 -0200

    crossover and mutation done

--- ag.py
+++ ag.py
@@ -45,35 +45,67 @@ def selection(population,pop_size):
 		rand = uniform(0.0,max(roullete))
 		for i in range(1,len(roullete)):
 			if(rand > roullete[i-1] and rand < roullete[i]):
-				selected_parents.append(i)
+				selected_parents.append(i-1)
 	return selected_parents
 
-def crossover(selected_parents, crossover_rate):
+def crossover(population, selected_parents, crossover_rate):
 	sons = []
-	for i in range(0,len(selected_parents)-1):
+	i = 0
+	while i < len(selected_parents):
 		alpha = random()
-		parents = [selected_parents[i],selected_parents[i+1]]
-		son1 = Cromossome(0,0,0)
-		son2 = Cromossome(0,0,0)
-		print(parents[0])
-		son1.x1 = alpha*parents[0].x1+(1-alpha)*parents[1].x1
-		son1.x2 = alpha*parents[0].x2+(1-alpha)*parents[1].x2
-		son2.x1 = alpha*parents[1].x1+(1-alpha)*parents[2].x1
-		son2.x2 = alpha*parents[1].x2+(1-alpha)*parents[2].x2
+		parents = [population[selected_parents[i]], population[selected_parents[i+1]]]
+		if(random() < crossover_rate):
+			son1 = Cromossome(0,0,0)
+			son2 = Cromossome(0,0,0)
+			son1.x1 = alpha*parents[0].x1+(1-alpha)*parents[1].x1
+			son1.x2 = alpha*parents[0].x2+(1-alpha)*parents[1].x2
+			son2.x1 = alpha*parents[1].x1+(1-alpha)*parents[0].x1
+			son2.x2 = alpha*parents[1].x2+(1-alpha)*parents[0].x2
+		else:
+			son1 = population[selected_parents[i]]
+			son2 = population[selected_parents[i+1]]
 		sons.append(son1)
 		sons.append(son2)
+		i = i+2
 	return sons
 
+def mutacao(population, mutation_rate):
+	for c in population:
+		if(random() < mutation_rate):
+			mutated_value = c.x1+uniform(-2.0,2.0)
+			if(not(mutated_value < -600)):
+				c.x1 = mutated_value
+			else:
+				c.x1 = -600
+			if(not(mutated_value > 600)):
+				c.x1 = mutated_value
+			else:
+				c.x1 = 600
+
+		if(random() < mutation_rate):
+			mutated_value = c.x2+uniform(-2.0,2.0)
+			if(not(mutated_value < -600)):
+				c.x2 = mutated_value
+			else:
+				c.x2 = -600
+			if(not(mutated_value > 600)):
+				c.x2 = mutated_value
+			else:
+				c.x2 = 600
+	return population
+
+def print_population(population):
+	for c in population:
+		print(str(c.x1)+" "+str(c.x2))
 
 pop_size = 100
 crossover_rate = 0.7
+mutation_rate = 0.1
 population = generate_population(pop_size)
-population = fitness(population)
-selected_parents = selection(population,pop_size)
-population = crossover(selected_parents,crossover_rate)
-print(population)
-
-# na mutacao e pra bloquear o numero 
-# no maximo se ele passar do dominio
-# da funcao
+generations = 30
+for i in range(0,generations):
+	population = fitness(population)
+	selected_parents = selection(population,pop_size)
+	population = crossover(population, selected_parents, crossover_rate)
+	population = mutacao(population,mutation_rate)
 
