98e870acc225bcc1113b7812c776359e15199706
Higor Coimbra <higor1308@gmail.com>
2017-11-07 13:31:46 -0200

    including statistical analysis

--- ag.py
+++ ag.py
@@ -1,5 +1,6 @@
 from random import random, uniform
 from math import sin, sqrt
+import matplotlib.pyplot as plt
 
 # offset of function to turn negative numbers into positive ones
 offset = 1500
@@ -7,6 +8,11 @@ offset = 1500
 # function domain
 min_domain = -600
 max_domain = 600
+
+# global variables to evaluate ga behavior
+max_fitness = 0
+min_fitness = 2500
+mid_fitness = 0
 
 class Cromossome:
 	def __init__(self, x1, x2,fitness):
@@ -25,9 +31,14 @@ def generate_population(pop_size):
 
 # generate fitness value for every cromossome in population
 def fitness(population):
+	mid_fitness = 0
 	for c in population:
 		c.fitness = (-c.x2+47)*sin(sqrt(abs(c.x2+(c.x1/2)+47)))-c.x1*sin(sqrt(abs(c.x1-(c.x2+47))))
 		c.fitness = -c.fitness+offset
+		max_fitness = max(c.fitness,max_fitness)
+		min_fitness = min(c.fitness,min_fitness)
+		mid_fitness += c.fitness 
+	mid_fitness /= len(population)
 	return population
 
 # selection process - roulette wheel
@@ -44,7 +55,7 @@ def selection(population,pop_size):
 	for i in range(0,pop_size):
 		rand = uniform(0.0,max(roullete))
 		for i in range(1,len(roullete)):
-			if(rand > roullete[i-1] and rand < roullete[i]):
+			if(rand > roullete[i-1] and rand <= roullete[i]):
 				selected_parents.append(i-1)
 	return selected_parents
 
@@ -103,9 +114,26 @@ crossover_rate = 0.7
 mutation_rate = 0.1
 population = generate_population(pop_size)
 generations = 30
+max_fitness_list = []
+mid_fitness_list = []
+min_fitness_list = []
 for i in range(0,generations):
 	population = fitness(population)
+
+	# array operations for statistical analysis
+	max_fitness_list.append(max_fitness)
+	min_fitness_list.append(min_fitness)
+	mid_fitness_list.append(mid_fitness)
+	
+	# plot for statistical analysis
+	plt.plot(max_fitness_list)
+	plt.plot(mid_fitness_list)
+	plt.plot(min_fitness_list)
+	plt.show()
+
+	# general process of ga
 	selected_parents = selection(population,pop_size)
 	population = crossover(population, selected_parents, crossover_rate)
 	population = mutacao(population,mutation_rate)
+
 
